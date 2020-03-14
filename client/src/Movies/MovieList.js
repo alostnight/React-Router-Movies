1781955099ab8dd84514649729de22c5c34461ba
Jose <alostnight1993@outlook.com>
2020-03-13 19:22:24 -0500

    finished mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import axios from 'axios';
 
 const MovieList = props => {
@@ -28,10 +29,12 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars, id } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link to = {`./movies/${id}`} >
+        <h2>{title}</h2>
+        </Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
